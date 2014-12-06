50d15f8219cc1ae5c0e755419e9bfbbee6aeb1c0
Aidan C. Brady <aidancbrady@aol.com>
2014-12-06 13:48:09 -0500

    Navigation work for Games List

---
+++ VocabCrack-iOS/NewGameController.swift
@@ -0,0 +1,41 @@
+//
+//  NewGameController.swift
+//  VocabCrack-iOS
+//
+//  Created by aidancbrady on 12/6/14.
+//  Copyright (c) 2014 aidancbrady. All rights reserved.
+//
+
+import UIKit
+
+class NewGameController: UIViewController
+{
+    @IBAction func backButton(sender: AnyObject)
+    {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
