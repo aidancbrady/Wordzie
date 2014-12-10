52c38b294b787aa0057c9e652b55f2094a80f50e
Aidan C. Brady <aidancbrady@aol.com>
2014-12-10 17:40:30 -0500

    Work on new game controller

--- VocabCrack-iOS/NewGameController.swift
+++ VocabCrack-iOS/NewGameController.swift
@@ -2,7 +2,7 @@
 //  NewGameController.swift
 //  VocabCrack-iOS
 //
-//  Created by aidancbrady on 12/6/14.
+//  Created by aidancbrady on 12/10/14.
 //  Copyright (c) 2014 aidancbrady. All rights reserved.
 //
 
@@ -10,6 +10,109 @@ import UIKit
 
 class NewGameController: UIViewController
 {
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var typeButton: UISegmentedControl!
+    @IBOutlet weak var playLabel: UILabel!
+    @IBOutlet weak var playButton: UISegmentedControl!
+    @IBOutlet weak var listLabel: UILabel!
+    @IBOutlet weak var listButton: UISegmentedControl!
+    @IBOutlet weak var urlField: UITextField!
+    @IBOutlet weak var finishedLabel: UILabel!
+    @IBOutlet weak var continueButton: UIButton!
+    
+    @IBAction func typePressed(sender: AnyObject)
+    {
+        if playLabel.hidden
+        {
+            playLabel.hidden = false
+            playButton.hidden = false
+            playLabel.alpha = 0.1
+            playButton.alpha = 0.1
+            UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+                self.playLabel.alpha = 1
+                self.playButton.alpha = 1
+            }, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func playPressed(sender: AnyObject)
+    {
+        if listLabel.hidden
+        {
+            listLabel.hidden = false
+            listButton.hidden = false
+            listLabel.alpha = 0.1
+            listButton.alpha = 0.1
+            UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+                self.listLabel.alpha = 1
+                self.listButton.alpha = 1
+            }, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func listPressed(sender: AnyObject)
+    {
+        if listButton.selectedSegmentIndex == 0
+        {
+            if !urlField.hidden
+            {
+                UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+                    self.urlField.alpha = 0
+                }, completion: {b in
+                    self.urlField.hidden = true
+                    self.urlField.text = ""
+                })
+            }
+            
+            if finishedLabel.hidden
+            {
+                finishedLabel.hidden = false
+                finishedLabel.alpha = 0.1
+                UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+                    self.finishedLabel.alpha = 1
+                }, completion: nil)
+            }
+        }
+        else {
+            if urlField.hidden
+            {
+                urlField.hidden = false
+                urlField.alpha = 0.1
+                UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+                    self.urlField.alpha = 1
+                }, completion: nil)
+            }
+            
+            if !finishedLabel.hidden
+            {
+                UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+                    self.finishedLabel.alpha = 0
+                }, completion: {b in
+                    self.finishedLabel.hidden = true
+                })
+            }
+        }
+    }
+    
+    @IBAction func continuePressed(sender: AnyObject)
+    {
+        
+    }
+    
+    override func viewDidAppear(animated: Bool)
+    {
+        typeLabel.hidden = false
+        typeButton.hidden = false
+        typeLabel.alpha = 0.1
+        typeButton.alpha = 0.1
+        UIView.transitionWithView(view, duration: 0.4, options: UIViewAnimationOptions.CurveEaseOut, animations: {() in
+            self.typeLabel.alpha = 1
+            self.typeButton.alpha = 1
+        }, completion: nil)
+    }
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -20,7 +123,6 @@ class NewGameController: UIViewController
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        
         // Dispose of any resources that can be recreated.
     }
 
@@ -33,4 +135,5 @@ class NewGameController: UIViewController
         // Pass the selected object to the new view controller.
     }
     */
+
 }
