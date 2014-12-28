79a13237a2358a1616ab148cd5f3c4dad09c0e40
Aidan C. Brady <aidancbrady@aol.com>
2014-12-28 14:29:21 -0500

    A bit of name refactoring

--- VocabCrack-iOS/Wordzie.swift
+++ VocabCrack-iOS/Wordzie.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class VocabCrack
+class Wordzie
 {
     var account:Account = Defaults.ACCOUNT
     var avatars:[String: UIImage] = [String: UIImage]()
@@ -35,10 +35,10 @@ struct Constants
     static var PORT:Int = 26830
     
     static let VERSION:String = "1.0.0"
-    static let DATA_URL:NSURL = NSURL(string: "https://dl.dropboxusercontent.com/u/90411166/Versions/VocabCrack.txt")!
+    static let DATA_URL:NSURL = NSURL(string: "https://dl.dropboxusercontent.com/u/90411166/Versions/Wordzie.txt")!
     static let BAD_CHARS:[String] = [SPLITTER_1, SPLITTER_2, "&", " ", "|", LIST_SPLITTER]
     static let BANNED_CHARS:[String] = [SPLITTER_1, SPLITTER_2, "&", "|", LIST_SPLITTER]
-    static let CORE:VocabCrack = VocabCrack()
+    static let CORE:Wordzie = Wordzie()
     
     static let LIST_SPLITTER:String = ">"
     static let SPLITTER_1:String = "}"
